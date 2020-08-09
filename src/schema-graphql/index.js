dbbbd45a764b1310623384eb567db26f5681049f
Dev <ehtishamali042@gmail.com>
2020-08-09 18:23:24 +0500

    old Schema mmehtod

--- src/schema-graphql/index.js
+++ src/schema-graphql/index.js
@@ -3,6 +3,7 @@ import { Mutation } from "./mutations";
 const graphql = require("graphql");
 const { GraphQLSchema } = graphql;
 
+//old schema method
 export const schema = new GraphQLSchema({
   query: Query,
   mutation: Mutation,
